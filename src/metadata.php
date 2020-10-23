ce6fe8549582215b927fb4e14a64ab5c565fc1bb
Daniel Seifert <ds@shopmodule.com>
2020-10-23 10:14:50 +0200

    add 4.0.2.0 (PHP 5.6 - 7.0)

--- src/metadata.php
+++ src/metadata.php
@@ -35,7 +35,7 @@ $aModule = array(
         'en' =>  '',
     ),
     'thumbnail'   => 'picture.png',
-    'version'     => '4.0.2.0',
+    'version'     => '4.0.2.1',
     'author'      => 'D&sup3; Data Development (Inh.: Thomas Dartsch)',
     'email'       => 'support@shopmodule.com',
     'url'         => 'http://www.oxidmodule.com/',
